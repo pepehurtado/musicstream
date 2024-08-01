32bf1cb4183a14c95349c29500cf461e2b0e9094
Jose Hurtado <pepeke2000@gmail.com>
2024-08-01 09:07:00 +0200

    feat: Add @CrossOrigin annotation to PermissionController

--- src/main/java/com/musicapp/musicstream/controller/PermissionController.java
+++ src/main/java/com/musicapp/musicstream/controller/PermissionController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.musicapp.musicstream.entities.Permission;
 import com.musicapp.musicstream.repository.PermissionRepository;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/permissions")
+@Tag(name = "Role", description = "Operations related to Permission")
 public class PermissionController {
 
     @Autowired
